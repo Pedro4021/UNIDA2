2a478ea8677e2495654d5eda7af416aa43cda4d6
pedro4012 <90282467+Pedro4021@users.noreply.github.com>
2021-10-19 14:20:09 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nombre="Pedro Uriel Rivero Enciso ";
+    echo $nombre."</br">;
+    echo "</br>";
+    $edad=19;
+    echo $edad;
+    $sexo="Hombre  ";
+    echo $sexo;
+    $hobbie1="leer ";
+    echo $hobbie1;
+    $hobbie2="caminar ";
+    echo $hobbie2;
+    $hobbie3="escuchar musica ";
+    echo $hobbie3;
+    $hobbie4="leer x2 ";
+    echo $hobbie4;
+    $hobbie5="ver peliculas ";
+    echo $hobbie5;
+    
+
+    
+    ?>
+    
+</body>
+</html>
